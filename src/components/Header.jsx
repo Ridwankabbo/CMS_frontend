6f06e333d188c3457ea593441b294407c5df53f3
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-12-23 22:39:53 +0600

    changing designe ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Header = ()=>{
     return(
-        <section className="flex justify-around items-center">
+        <section className="flex justify-around items-center bg-blue-600 text-white py-5">
 
             <div>
-                <img src="images/siteBuilder_logo.jpg" className="h-30 w-30" alt="logo" />
+                {/* <img src="images/siteBuilder_logo.jpg" className="h-25 w-25" alt="logo" /> */}
+                <h2 className="text-4xl font-bold">Site Builder</h2>
             </div>
             <div>
                 <ul className="flex gap-10 font-semibold">
--- src/components/dashboardNavBar.jsx
+++ src/components/dashboardNavBar.jsx
@@ -4,7 +4,7 @@ const DashboardNavBar = () =>{
     return(
         <div className="flex justify-around bg-green-600 py-5 text-white">
             <div className="flex items-center text-4xl font-bold gap-5">
-                <img src="images/siteBuilder_logo.jpg" alt="" height='70px' width='100px' />
+                {/* <img src="images/siteBuilder_logo.jpg" alt="" height='70px' width='100px' /> */}
                 <h2>Site Builder</h2>
             </div>
             <div>
